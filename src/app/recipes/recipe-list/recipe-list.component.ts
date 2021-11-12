82a92f4dca4da3b84831d87ffb88969b9962fbe7
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-12 12:34:55 +0500

    Manage Recipe and Shopping list indiviually.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,14 +6,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.licious.in/blog/wp-content/uploads/2020/12/Roast-Chicken-600x600.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply another test', 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png')
-  ];
+  @Output() wasSelectedRecipe = new EventEmitter<Recipe>();
+  recipes = [
+    {name:'A Test Recipe', description:'This is simply a test', imagePath:'https://www.licious.in/blog/wp-content/uploads/2020/12/Roast-Chicken-600x600.jpg'},
+    {name:'Another Test Recipe', description:'This is simply another test', imagePath:'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png'}
+    ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.wasSelectedRecipe.emit(recipe);
   }
 
 }

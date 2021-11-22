b30b305e888daacae90172fe041b1ed289ac15c3
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-23 00:48:23 +0500

    Authentication using Ngrx and effects for side effects like local storage, redirection, HTTP requests.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
@@ -7,6 +8,7 @@ import { AuthComponent } from "./auth.component";
 @NgModule({
     declarations: [AuthComponent],
     imports: [
+        CommonModule,
         SharedModule, 
         FormsModule, 
         RouterModule.forChild([{ path: '', component: AuthComponent}])
